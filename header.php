5e74f8410483ac7b8ffce34d7753def738a53412
Guillermo <49420727+AlexbyX@users.noreply.github.com>
2023-05-11 16:26:12 -0600

    Correccion de url amigables
    correcion en headre.php, href modificados para las nuevas rutas de acceso.

--- header.php
+++ header.php
@@ -24,25 +24,25 @@ if (isset($message)) {
                 <a href="#" style="text-decoration: none;" class="fab fa-instagram"></a>
                 <a href="#" style="text-decoration: none;" class="fab fa-linkedin"></a>
             </div>
-            <p> <a href="login.php" style="text-decoration: none;">Acceder</a> | <a href="register.php" style="text-decoration: none;">Registrarse</a> </p>
+            <p> <a href="login" style="text-decoration: none;">Acceder</a> | <a href="register" style="text-decoration: none;">Registrarse</a> </p>
         </div>
     </div>
 
     <div class="header-2">
         <div class="flex">
-            <a href="home.php" style="text-decoration: none;" class="logo">El Rincón del Taco</a>
+            <a href="home" style="text-decoration: none;" class="logo">El Rincón del Taco</a>
 
             <nav class="navbar">
-                <a href="home.php" style="text-decoration: none;">Inicio</a>
-                <a href="about.php" style="text-decoration: none;">Acerca de</a>
-                <a href="shop.php" style="text-decoration: none;">Menú</a>
-                <a href="contact.php" style="text-decoration: none;">Contáctanos</a>
-                <a href="orders.php" style="text-decoration: none;">Órdenes</a>
+                <a href="home" style="text-decoration: none;">Inicio</a>
+                <a href="about" style="text-decoration: none;">Acerca de</a>
+                <a href="shop" style="text-decoration: none;">Menú</a>
+                <a href="contact" style="text-decoration: none;">Contáctanos</a>
+                <a href="orders" style="text-decoration: none;">Órdenes</a>
             </nav>
 
             <div class="icons">
                 <div id="menu-btn" class="fas fa-bars"></div>
-                <a href="search_page.php" style="text-decoration: none;" class="fas fa-search"></a>
+                <a href="search_page" style="text-decoration: none;" class="fas fa-search"></a>
                 <div id="user-btn" class="fas fa-user"></div>
                 <?php
 
@@ -55,13 +55,13 @@ if (isset($message)) {
                 }
 
                 ?>
-                <a href="cart.php" style="text-decoration: none;"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $counter; ?>)</span> </a>
+                <a href="cart" style="text-decoration: none;"> <i class="fas fa-shopping-cart"></i> <span>(<?php echo $counter; ?>)</span> </a>
             </div>
 
             <div class="user-box">
                 <p>Usuario : <span><?php echo $_SESSION['user_name']; ?></span></p>
                 <p>Correo : <span><?php echo $_SESSION['user_email']; ?></span></p>
-                <a href="logout.php" class="delete-btn" style="text-decoration: none;">Salir</a>
+                <a href="logout" class="delete-btn" style="text-decoration: none;">Salir</a>
             </div>
         </div>
     </div>
