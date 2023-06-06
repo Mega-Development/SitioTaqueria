9f5cc6cac8b70ab8543050a7ea72a9be69d4ea54
AdelsGuerr24 <adelso.guerra@outlook.com>
2023-06-05 23:33:09 -0600

    Refresco de caché

--- header.php
+++ header.php
@@ -1,30 +1,36 @@
 <?php
-    require 'vendor/autoload.php';
-    include 'db_connection.php';
-    if(isset($message)){
-        foreach($message as $message){
-            echo '
+require 'vendor/autoload.php';
+include 'db_connection.php';
+if (isset($message)) {
+    foreach ($message as $message) {
+        echo '
             <div class="message">
-                <span>'.$message.'</span>
+                <span>' . $message . '</span>
                 <a class="btn" href="cart.php">Ver Carrito</a>
                 <i class="fa-solid fa-circle-check"></i>
                 <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
             </div>
         ';
-        }
     }
+}
 ?>
 
 <header class="header">
+    <!-- Caché -->
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    
     <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-   <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
 
     <div class="header-2">
         <div class="flex">
-        
+
             <nav class="navbar">
-                <a class="logo" href="home.php"><img src="./images/Logo_Taquería.png" width="125px"  alt="#" /></a>
+                <a class="logo" href="home.php"><img src="./images/Logo_Taquería.png" width="125px" alt="#" /></a>
                 <a href="home.php">Inicio</a>
                 <a href="about.php">Acerca de</a>
                 <a href="shop.php">Menú</a>
@@ -41,9 +47,8 @@
                 $carrito = $cart->find(
                     ['user_id' => new MongoDB\BSON\ObjectId($user_id)]
                 );
-                $counter=0;
-                foreach($carrito as $doc)
-                {
+                $counter = 0;
+                foreach ($carrito as $doc) {
                     $counter++;
                 }
 
